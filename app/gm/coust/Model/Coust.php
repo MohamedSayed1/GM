49ce59bdda76bf65639e1673001a3f8fe144c895
Mohamed Sayed <msayed0@outlook.com>
2019-03-22 18:14:42 +0200

    start Coust

---
+++ app/gm/coust/Model/Coust.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: msaye
+ * Date: 3/22/2019
+ * Time: 6:13 PM
+ */
+
+namespace App\gm\coust\Model;
+
+
+use Illuminate\Database\Eloquent\Model;
+
+class Coust extends Model
+{
+
+}
